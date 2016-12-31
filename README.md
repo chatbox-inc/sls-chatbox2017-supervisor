94ad07f1ab1f45027d522c502207bf4ab02fefad
t.goto <t.goto@chatbox-inc.com>
2016-12-31 18:05:11 +0900

    update

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# Slack Sns 連携
+# Serverless 時報アプリケーション
 
-Slack と Sns をつなぎます。
-
-複数の ServerlessBot でSlackBotとか作る時便利です。
 
 以下のコマンドでデプロイ出来ます。
 
 ````
-SLS_SLACK_URL=https://hooks.slack.com/services/XXX/XXX/XXXXX ./node_modules/.bin/sls deploy -v
+./node_modules/.bin/sls deploy -v
 ````
 
---
+++ src/jiho.js
@@ -0,0 +1,65 @@
+'use strict';
+const AWS = require('aws-sdk')
+const sns = new AWS.SNS()
+
+const moment = require("moment-timezone")
+
+const textList = {
+    9 : "おはようございます。\n9時をお知らせします。\n",
+    10 : "10時をお知らせします。\n今日も1日頑張りましょう！",
+    11 : "11時をお知らせします。",
+    12 : "12時をお知らせします。",
+    13 : "進捗はいかがですか？\n13時になりました。\n",
+    14 : "14時です。\nお昼はもうとりましたか？",
+    15 : "15時をお知らせします。",
+    16 : "16時をお知らせします。",
+    17 : "17時です。\n進捗をまとめて帰宅の準備を始めましょう。",
+    18 : "18時をお知らせします。",
+    19 : "お疲れ様でした！19時です。\n明日も1日よろしくお願いします。",
+    20 : "20時をお知らせします。",
+}
+
+class Jiho{
+
+    constructor(topicArn)
+    {
+        this.topicArn = topicArn;
+    }
+
+    getMessage(text)
+    {
+        return {
+            channel: "#chatbox",
+            text
+        }
+    }
+
+    getDoneCb(cb)
+    {
+        return (err, result) => cb(null, {
+            statusCode: err ? '500' : '200',
+            body: err ? err.message : JSON.stringify(result),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    }
+
+    run(event, context, cb)
+    {
+        const hour = moment().tz("Asia/Tokyo").hour()
+        const text = textList[hour]
+
+        if(text){
+            sns.publish({
+                Message: this.getMessage(),
+                Subject: "SLS JIHO APPLICATION",
+                TopicArn: this.topicArn
+            }, getDoneCb(cb))
+        }else{
+            callback(null)
+        }
+    }
+}
+
+module.exports = Jiho
